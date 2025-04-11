b204e50b6c2dd8f0394ece31e803512da13904e7
Dmytro Boiko <iamboikodmytro@gmail.com>
2025-04-11 18:18:41 +0300

    my first test

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,16 @@
+/// <reference types="cypress"/>
+
+describe('My first test', () => {
+  it('should', () => {
+    // Заходим на сайт и проверяем есть ли там заголовок h1 с нужным текстом
+    cy.visit('https://react-redux.realworld.io/')
+      .get('h1')
+      .should('contain.text', 'conduit');
+
+    // Ищем элемент с текстом Sign in и нажимаем на него
+    cy.contains('Sign in').click();
+
+    // Проверяем, есть ли на сайте нужный элемент с текстом
+    cy.get('h1').should('contain.text', 'Sign In');
+  });
+});
