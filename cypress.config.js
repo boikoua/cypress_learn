06eb3726d7b5ae05993d712d4b033a937134378f
Dmytro Boiko <iamboikodmytro@gmail.com>
2025-04-12 09:50:18 +0300

    tests for all pages

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,6 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://react-redux.realworld.io/',
-    viewportWidth: 500,
-    viewportHeight: 700,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
---
+++ cypress/e2e/home.page.spec.cy.js
@@ -0,0 +1,48 @@
+/// <reference types="cypress"/>
+
+describe('My home page', () => {
+  // то что используем перед каждым тестом
+  beforeEach(() => {
+    // Отключаем падение теста при ошибке на стороне приложения
+    Cypress.on('uncaught:exception', (err, runnable) => {
+      // Возвращаем false, чтобы Cypress не падал
+      return false;
+    });
+
+    // переходим на главную страницу сайта перед каждым новым тестом
+    cy.visit('/');
+  });
+
+  it('should have all parts', () => {
+    // Заходим на сайт и проверяем есть ли там заголовок h1 с нужным текстом
+    cy.get('h1').should('contain.text', 'conduit');
+
+    // Ищем ссылку с текстом, exist значит что должна быть обязательно
+    cy.contains('a', 'Global Feed').should('exist');
+
+    // Ищем по селектору класса текстом, exist значит что должна быть обязательно
+    cy.contains('.sidebar', 'Popular Tags').should('exist');
+  });
+
+  it('should click on Sign In', () => {
+    // Ищем элемент с текстом Sign in и нажимаем на него
+    cy.contains('a', 'Sign in').should('exist').click();
+
+    // Проверяем поменялся ли адрес сайта
+    cy.url().should('include', '/login');
+
+    // Проверяем, есть ли на сайте нужный элемент с текстом
+    cy.get('h1').should('contain.text', 'Sign In');
+  });
+
+  it('should click on Sign Up', () => {
+    // Ищем элемент с текстом Sign in и нажимаем на него
+    cy.contains('a', 'Sign up').should('exist').click();
+
+    // Проверяем поменялся ли адрес сайта
+    cy.url().should('include', '/register');
+
+    // Проверяем, есть ли на сайте нужный элемент с текстом
+    cy.get('h1').should('contain.text', 'Sign Up');
+  });
+});
